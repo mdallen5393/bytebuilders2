cb0f2cca939798701d2c9de3a4e21bdd8ef0e38c
mdallen5393 <allen_matthew@outlook.com>
2024-06-12 22:56:12 -0500

    fixing index.js fixing index.js cuz github mistakes :|

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,45 @@
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
+const path = require("path");
+const dotenv = require("dotenv");
 
-// update this to use the desired email account later; make a new app password for that account
+// Load environment variables from .env file for local development
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: path.resolve(__dirname, ".env") });
+}
+
+// Access environment variables
+const email = process.env.GMAIL_ADDRESS || functions.config().email.address;
+const password = process.env.GMAIL_APP_PASSWORD || functions.config().email.password;
+
+if (!email || !password) {
+  throw new Error("Missing email or password environment variables.");
+}
+
+console.log("Email: ", email);
+console.log("Password: ", password ? "Loaded" : "Not Loaded");
+
+// Configure email transport using default SMTP transport and a Gmail account
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "itsmatthewallen1@gmail.com",
-    pass: "acjf zmri bkut dszb",
+    user: email,
+    pass: password,
   },
 });
 
 exports.sendContactEmail = functions.https.onCall((data, context) => {
   const mailOptions = {
     from: data.email,
-    to: "itsmatthewallen1@gmail.com",
+    to: email,
     subject: `Contact form submission from ${data.name} (${data.email})`,
     text: data.message,
   };
 
   return transporter.sendMail(mailOptions)
       .then(() => {
-        return {success: true};
+        return { success: true };
       })
       .catch((error) => {
-        return {success: false, error: error.toString()};
-      });
-});
+        return { success: false, error: error.toString() };
+
